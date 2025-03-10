89faab974410132827b12d5bcce2e9c29d519833
AKARSH JHA <129477999+Akarshjha03@users.noreply.github.com>
2025-03-10 22:15:37 +0530

    Update LeetCodeTwoSum.java

--- Arrays/LeetCodeTwoSum.java
+++ Arrays/LeetCodeTwoSum.java
@@ -10,3 +10,25 @@ class Solution {
         return new int[]{}; // Return empty if no solution is found (though it's guaranteed to exist)
     }
 }
+
+/*How Does LeetCode Call This Code?
+LeetCode internally runs something like this:
+
+java
+Copy
+Edit
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {2, 7, 11, 15}; 
+        int target = 9;
+
+        int[] result = solution.twoSum(nums, target);
+        System.out.println(Arrays.toString(result)); // Expected Output: [0, 1]
+    }
+}
+This main method is written by LeetCode internally to test your function.
+It creates an instance of Solution and calls twoSum(nums, target).
+nums = {2, 7, 11, 15} and target = 9 are passed as arguments.*/
+

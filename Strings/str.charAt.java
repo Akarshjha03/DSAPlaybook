2a59b47d20962c6e662a7c3648c93d8fd2919b62
AKARSH JHA <129477999+Akarshjha03@users.noreply.github.com>
2025-05-18 17:58:23 +0530

    Create str.charAt.java

---
+++ Strings/str.charAt.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String args[]){
+        String Name = "AKARSH";
+        String Surname = "JHA";
+        String Fullname = Name + " " + Surname;
+        
+        System.out.println(Fullname);
+        for(int i = 0; i < Fullname.length(); i++){
+            System.out.println(Fullname.charAt(i));
+        }
+        
+    }
+    
+}

bb94a3b96999b0926a4f0fba15bfda02f33f1b86
AKARSH JHA <129477999+Akarshjha03@users.noreply.github.com>
2025-05-18 17:34:55 +0530

    Create StrLength.java

---
+++ Strings/StrLength.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String args[]){
+        String Name = "AKARSH";
+        String Surname = "JHA";
+        String Fullname = Name + " " + Surname;
+        
+        System.out.println(Fullname);
+        System.out.println(Fullname.length());
+        
+    }
+    
+}

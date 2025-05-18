2eb1f83486945a658db248de0e3d6c98af924d34
AKARSH JHA <129477999+Akarshjha03@users.noreply.github.com>
2025-05-18 21:42:02 +0530

    Create StrSubstring.java

---
+++ Strings/StrSubstring.java
@@ -0,0 +1,11 @@
+public class StrSubstring{
+    public static void main(String args[]){
+        String Name = "Akarsh Jha";
+        
+        System.out.println(Name.substring(0,7));
+        System.out.println(Name.substring(7));
+        System.out.println(Name.substring(5,9));
+        
+    }
+    
+}

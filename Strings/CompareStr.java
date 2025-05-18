6d46bdb90f6d754ab3f30885b234f239698a748a
AKARSH JHA <129477999+Akarshjha03@users.noreply.github.com>
2025-05-18 21:19:45 +0530

    Create CompareStr.java

---
+++ Strings/CompareStr.java
@@ -0,0 +1,12 @@
+public class CompareStr{
+    public static void main(String args[]){
+        String s1 = "apple";
+        String s2 = "Banana";
+        
+        if(s1.compareTo(s2) == 0){
+            System.out.println("equal");
+        }else{
+            System.out.println("Not Equal");
+        }
+    }
+}

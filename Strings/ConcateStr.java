3ad15501ef019d45416d3b072eb391d095184f52
AKARSH JHA <129477999+Akarshjha03@users.noreply.github.com>
2025-05-18 17:33:19 +0530

    Create ConcateStr.java

---
+++ Strings/ConcateStr.java
@@ -0,0 +1,10 @@
+public class ConcateStr{
+    public static void main(String args[]){
+        String Name = "AKARSH";
+        String Surname = "JHA";
+        String Fullname = Name + " " + Surname;
+        
+        System.out.println(Fullname);
+    }
+    
+}

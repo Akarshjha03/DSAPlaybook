f1c18f2a5a5e9b4110102239f6f0b918a3d4df52
AKARSH JHA <129477999+Akarshjha03@users.noreply.github.com>
2025-05-18 21:25:04 +0530

    Create Comparestr2.java

---
+++ Strings/Comparestr2.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String args[]){
+        String s1 = "apple";
+        String s2 = "Banana";
+        
+        System.out.println(s1.compareTo(s2));
+        System.out.println("apple".compareToIgnoreCase("Banana")); // Output: -1
+
+    }
+    
+}

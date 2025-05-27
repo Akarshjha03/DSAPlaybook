c40e4379a7818fee703cff49c9cda2518572bcef
AKARSH JHA <129477999+Akarshjha03@users.noreply.github.com>
2025-05-27 12:51:46 +0530

    Create UserInput.java

---
+++ 1.Basics/UserInput.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class UserInput{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the element:");
+        int n = sc.nextInt();
+        System.out.println(n);
+    }
+}

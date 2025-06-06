4e08d20bbf34944cb915e75d576f9c16c3832b24
AKARSH JHA <129477999+Akarshjha03@users.noreply.github.com>
2025-06-06 11:08:12 +0530

    Update Pyramid.java

--- 3.Patterns/Pyramid.java
+++ 3.Patterns/Pyramid.java
@@ -2,13 +2,13 @@ public class Pyramid{
     public static void main(String args[]){
         int n = 5;
         for(int i = 1; i <= n ; i++){
-            for(int j = 1; j <= n-i; j++){
+            for(int j = 1; j <= n-i; j++){   //spaces 
                 System.out.print(" ");
             }
-            for(int k = 1; k <= (2 * i -1); k++){
+            for(int k = 1; k <= (2 * i -1); k++){   //stars
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println();  //rows
         }
         
     }

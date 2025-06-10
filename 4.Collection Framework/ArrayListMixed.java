706ead3ec5a7a00e6957103e7b133d5497fb5ad5
AKARSH JHA <129477999+Akarshjha03@users.noreply.github.com>
2025-06-10 11:09:23 +0530

    Create ArrayListMixed.java

---
+++ 4.Collection Framework/ArrayListMixed.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ArrayListMixed{
+    public static void main(String args[]){
+        ArrayList ar = new ArrayList();
+        ar.add("Akarsh");
+        ar.add("Jha");
+        ar.add("Backend Engineeer");
+        ar.add(true);
+        ar.add(22);
+        System.out.println(ar);
+    }
+}

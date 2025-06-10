73c9dbeae3f794b20a0af917534b657ecbde46d0
AKARSH JHA <129477999+Akarshjha03@users.noreply.github.com>
2025-06-10 11:07:25 +0530

    Create ArrayListGeneric.java

---
+++ 4.Collection Framework/ArrayListGeneric.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ArrayListGeneric{
+    public static void main(String args[]){
+        ArrayList<String> ar = new ArrayList<>();
+        ar.add("Akarsh");
+        ar.add("Jha");
+        ar.add("Backend Engineeer");
+        System.out.println(ar);
+    }
+}

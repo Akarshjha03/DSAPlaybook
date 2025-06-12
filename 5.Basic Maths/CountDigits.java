b21cbd19f2b3d4edbf24f0d4063969d8bed18912
AKARSH JHA <129477999+Akarshjha03@users.noreply.github.com>
2025-06-12 23:26:23 +0530

    Update CountDigits.java

--- 5.Basic Maths/CountDigits.java
+++ 5.Basic Maths/CountDigits.java
@@ -1,1 +1,9 @@
-
+public class CountDigits{
+    public static void main(String args[]){
+        int n = 12345;
+      // Convert number to string and then to char array
+        char digits[] = String.valueOf(n).toCharArray(); //first the value is converted to string by "12345" then character by character to array
+        
+        System.out.println("Number of digits are:" + digits.length); //printing the length of array
+    }
+}

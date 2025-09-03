9dc5af146b82f96b5ab116602e40975b4344e9ce
AKARSH JHA <129477999+Akarshjha03@users.noreply.github.com>
2025-09-03 22:15:40 +0530

    Update CountDigits.java

--- 5.Basic Maths/CountDigits.java
+++ 5.Basic Maths/CountDigits.java
@@ -7,3 +7,29 @@ public class CountDigits{
         System.out.println("Number of digits are:" + digits.length); //printing the length of array
     }
 }
+
+//------------------------------------------------------------OR---------------------------------------------------------------------------------------
+
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the elements: ");
+        int n = sc.nextInt();
+        
+        int count = 0;
+        int temp = Math.abs(n); //Handles Negative Values
+        
+        if(temp == 0){
+            count = 1; //if the value is 0, then returns only 1
+        }
+        else{
+            while(temp > 0){
+                temp /= 10; //Just chop digits off
+                count++;
+            }
+        }
+        System.out.println("Count is: " + count);
+    }
+}
